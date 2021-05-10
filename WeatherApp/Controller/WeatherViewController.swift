634c38e04a9110117329301094b327e884e802f8
Uday <udayabhihek.in@gmail.com>
2021-05-10 18:26:44 +0530

    initial setup: * added MVC Group * added image background * customised weather VC

---
+++ WeatherApp/Controller/WeatherViewController.swift
@@ -0,0 +1,33 @@
+//
+//  WeatherViewController.swift
+//  WeatherApp
+//
+//  Created by Uday Kumar Abhishek . (Service Transformation) on 10/05/21.
+//
+
+import UIKit
+
+class WeatherViewController: UIViewController {
+    
+    @IBOutlet weak var labelCity: UILabel!
+    @IBOutlet weak var labelTemperature: UILabel!
+    @IBOutlet weak var imageViewWeather: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
